1efbe565c7f98fd0763b44dc121046ac1ac1c6d0
fernandohdsb <fernandohdsb1@gmail.com>
2025-12-05 16:28:10 +0100

    AD

--- Categorias/app/src/main/java/com/example/categorias/model/Categoria.kt
+++
@@ -1,4 +0,0 @@
-package com.example.categorias.model
-
-class Categoria(var categoria: String, var cartel: Int) {
-}
--- Categorias/app/src/main/java/com/example/categorias/model/Producto.kt
+++
@@ -1,6 +0,0 @@
-package com.example.categorias.model
-
-import java.io.Serializable
-
-class Producto(var nombre: String, var precio: Int, var imagen: String, var stock: Int, var descripcion: String, var categoria: String ) : Serializable {
-}
